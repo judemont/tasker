afde01862a20bd2a9a57771329430ee913db19f3
Rdemont <rdemont@rmbi.ch>
2024-03-02 18:58:39 +0100

    separete UI

---
+++ lib/screens/groupWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroupWidget extends StatefulWidget {
+  const GroupWidget({super.key});
+
+  @override
+  State<GroupWidget> createState() => _GroupWidgetState();
+}
+
+class _GroupWidgetState extends State<GroupWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
